4b2af908d541b5f66e0ef29b2adfb5d9400f6ec4
Michael Brown <Vurclash@gmail.com>
2019-05-13 21:51:02 -0600

    added edit and delete

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -20,7 +20,29 @@ class NotesController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update 
+    @note = Note.find(params[:id])
+
+    if @note.update(note_params)
+      redirect_to notes_path
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy
+    redirect_to notes_path
+  end
+
+  private 
+
+  def pages_params
+    params.require(:page).permit(:title, :author, :body)
+  end
 end
-
-
-
